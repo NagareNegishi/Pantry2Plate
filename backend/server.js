e7e73897122757acca8db95394cb89f05e1171b2
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-30 15:29:05 +1300

    backend/server.js setup #1

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
