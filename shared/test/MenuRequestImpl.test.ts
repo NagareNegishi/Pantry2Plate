ca753a67e54778d1eb52baa37e5394036bd976e7
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-04 19:26:10 +1300

    test only impl version #2

---
+++ shared/test/MenuRequestImpl.test.ts
@@ -0,0 +1,23 @@
+// tests/MenuRequestImpl.test.ts
+
+// Import what to test
+import { MenuRequestImpl } from '../src/MenuRequest';
+
+// Jest test syntax:
+// test(name, fn, timeout)
+// Also under the alias: it(name, fn, timeout)
+// So test == it
+
+describe('MenuRequestImpl', () => {
+  describe('Constructor', () => {
+    it('should set default servings to 1', () => {
+      const request = new MenuRequestImpl({ ingredients: ['rice'] });
+      expect(request.servings).toBe(1);
+    });
+    
+    it('should set default mealType to any', () => {
+      const request = new MenuRequestImpl({ ingredients: ['rice'] });
+      expect(request.mealType).toBe('any');
+    });
+  });
+});
