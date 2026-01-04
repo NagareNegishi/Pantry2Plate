c66278623eb541558a4f5e37c74e57a7610f035b
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-04 19:38:14 +1300

    only need impl test #2

---
+++ shared/test/MenuResponseImpl.test.ts
@@ -0,0 +1,10 @@
+// // tests/MenuResponseImpl.test.ts
+
+// Import what to test
+
+// fake temp test
+describe('MenuResponseImpl', () => {
+  it('should create an instance with default values', () => {
+
+  });
+});
