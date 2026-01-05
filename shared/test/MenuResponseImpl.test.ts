04eede6414f79db9b9fc634a24c478dd6b2961d5
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-05 23:17:19 +1300

    initial setup #2

--- shared/test/MenuResponseImpl.test.ts
+++ shared/test/MenuResponseImpl.test.ts
@@ -1,11 +1,14 @@
 // // tests/MenuResponseImpl.test.ts
-import { describe, it } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
+import { MenuResponseImpl } from '../src/MenuResponse';
 
 // Import what to test
 
 // fake temp test
 describe('MenuResponseImpl', () => {
   it('should create an instance with default values', () => {
+    const response = new MenuResponseImpl({});
+    expect(response.menus).toEqual([]);
 
   });
 });
