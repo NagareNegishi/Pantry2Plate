6aa7b0182a39a01993dbe2c20d1dd7ba4949ecb5
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-12 19:04:47 +1300

    initial #2

--- backend/src/services/claude.service.ts
+++ backend/src/services/claude.service.ts
@@ -1,1 +1,19 @@
-// Claude API calls
+/**
+ * backend/src/services/claude.service.ts
+ * Claude API calls: https://platform.claude.com/docs/en/api/overview
+ * Anthropic TypeScript API Library: https://github.com/anthropics/anthropic-sdk-typescript
+ */
+
+import Anthropic from '@anthropic-ai/sdk';
+
+const client = new Anthropic({
+  apiKey: process.env['ANTHROPIC_API_KEY'], // This is the default and can be omitted
+});
+
+const message = await client.messages.create({
+  max_tokens: 1024,
+  messages: [{ role: 'user', content: 'Hello, Claude' }],
+  model: 'claude-sonnet-4-5-20250929',
+});
+
+console.log(message.content);
