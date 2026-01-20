6d392e36002510acb09b5cb73387b3270f6c6aa3
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-20 17:05:18 +1300

    jsdom #2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest/config" />
+/**
+ * If you are already using Vite, add test property in your Vite config.
+ * Need to add a reference to Vitest types using a triple slash directive at the top of your config file.
+ */
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    // globals: true,
+    // setupFiles: './src/setupTests.ts',
+  },
 })
