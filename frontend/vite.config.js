8d5a89a8c4a473061dbc451946c1688a1aa5deee
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-20 19:21:14 +1300

    tailwind setup #3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   test: {
     environment: 'jsdom',
     // globals: true, // if I want to skip importing 'describe', 'it', 'expect' in each test file
