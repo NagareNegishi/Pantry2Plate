0ea8bdf572ab8071df098faac52d47acae05609e
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-23 17:42:19 +1300

    COMMON_INGREDIENTS list #3

---
+++ frontend/src/constants/ingredients.ts
@@ -0,0 +1,77 @@
+/**
+ * A list of common ingredients to be used in the IngredientsList component.
+ * This list can be used for autocomplete suggestions or as a reference.
+ */
+export const COMMON_INGREDIENTS = [
+  // Proteins - Meat (20)
+  'bacon', 'beef', 'chicken', 'chicken breast', 'chicken thighs',
+  'duck', 'ground beef', 'ground pork', 'ground turkey', 'ham',
+  'lamb', 'pork', 'pork chops', 'sausage', 'steak',
+  'turkey', 'veal', 'venison', 'prosciutto', 'salami',
+  
+  // Proteins - Seafood (15)
+  'cod', 'crab', 'halibut', 'lobster', 'mussels',
+  'salmon', 'scallops', 'shrimp', 'tilapia', 'tuna',
+  'anchovies', 'clams', 'mackerel', 'sardines', 'squid',
+  
+  // Proteins - Plant-based & Eggs (8)
+  'eggs', 'tofu', 'tempeh', 'seitan', 'edamame',
+  'egg whites', 'quail eggs', 'duck eggs',
+  
+  // Vegetables (45)
+  'artichoke', 'arugula', 'asparagus', 'avocado', 'bean sprouts',
+  'beets', 'bell pepper', 'bok choy', 'broccoli', 'brussels sprouts',
+  'cabbage', 'carrots', 'cauliflower', 'celery', 'corn',
+  'cucumber', 'eggplant', 'fennel', 'garlic', 'ginger',
+  'green beans', 'kale', 'leeks', 'lettuce', 'mushrooms',
+  'okra', 'onion', 'peas', 'potatoes', 'pumpkin',
+  'radish', 'red onion', 'scallions', 'shallots', 'snow peas',
+  'spinach', 'squash', 'sweet potato', 'tomato', 'turnip',
+  'watercress', 'yam', 'zucchini', 'jalapeno', 'chili pepper',
+  
+  // Fruits (12)
+  'apple', 'banana', 'lemon', 'lime', 'mango',
+  'orange', 'pear', 'pineapple', 'strawberries', 'blueberries',
+  'coconut', 'dates',
+  
+  // Grains & Starches (15)
+  'bread', 'breadcrumbs', 'couscous', 'flour', 'oats',
+  'pasta', 'quinoa', 'rice', 'brown rice', 'noodles',
+  'rice noodles', 'ramen', 'soba noodles', 'udon noodles', 'tortillas',
+  
+  // Legumes & Beans (10)
+  'black beans', 'chickpeas', 'kidney beans', 'lentils', 'pinto beans',
+  'white beans', 'navy beans', 'split peas', 'cannellini beans', 'lima beans',
+  
+  // Dairy & Alternatives (12)
+  'butter', 'cheese', 'cream', 'cream cheese', 'milk',
+  'mozzarella', 'parmesan', 'sour cream', 'yogurt', 'feta',
+  'cheddar', 'almond milk',
+  
+  // Nuts & Seeds (8)
+  'almonds', 'cashews', 'peanuts', 'walnuts', 'pecans',
+  'sesame seeds', 'sunflower seeds', 'pine nuts',
+  
+  // Herbs (15)
+  'basil', 'bay leaves', 'cilantro', 'dill', 'mint',
+  'oregano', 'parsley', 'rosemary', 'sage', 'thyme',
+  'chives', 'tarragon', 'marjoram', 'lemongrass', 'coriander',
+  
+  // Spices (12)
+  'black pepper', 'cinnamon', 'cumin', 'curry powder', 'paprika',
+  'red pepper flakes', 'salt', 'turmeric', 'cayenne', 'nutmeg',
+  'cardamom', 'cloves',
+  
+  // Condiments & Sauces (15)
+  'hot sauce', 'ketchup', 'mayonnaise', 'mustard', 'soy sauce',
+  'vinegar', 'worcestershire sauce', 'balsamic vinegar', 'fish sauce', 'honey',
+  'maple syrup', 'sriracha', 'tahini', 'teriyaki sauce', 'hoisin sauce',
+  
+  // Oils & Fats (6)
+  'olive oil', 'vegetable oil', 'coconut oil', 'sesame oil',
+  'canola oil', 'peanut oil',
+  
+  // Baking & Pantry (7)
+  'baking powder', 'baking soda', 'sugar', 'brown sugar', 'vanilla extract',
+  'cornstarch', 'yeast',
+];
