2b1a04bfaa3b48976955a4f582d4ac5bfe3dbf60
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-01-30 20:57:32 +1300

    Rendering test #9

--- frontend/src/components/ServingsInput.test.tsx
+++ frontend/src/components/ServingsInput.test.tsx
@@ -1,23 +1,34 @@
-// import { render } from '@testing-library/react';
-// import { describe, expect, it } from 'vitest';
-// import ServingsInput from './ServingsInput';
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { ServingsInput } from './ServingsInput';
 
-// describe('ServingsInput', () => {
-//   // 1. Rendering
-//   - [ ] Renders with initial value
-//   - [ ] Shows correct label
+describe('ServingsInput', () => {
+  // 1. Rendering
+	it('renders with provided value and correct label', () => {
+		// create fake DOM for testing
+		const { getByLabelText } = render(
+			<ServingsInput value={4} onChange={() => {}} />
+		);
+		
+		// get input by label, it will search all Labels elements and find the one with text "Servings"
+		// then it will use the htmlFor attribute to find the associated input element
+		const input = getByLabelText("Servings") as HTMLInputElement; // TypeScript needs this cast to access .value property
+		
+		expect(input.value).toBe('4');
+	});
 
-//   // 2. Valid input
-//   - [ ] Calls onChange when typing valid number (5)
-//   - [ ] Updates display value immediately
+  // // 2. Valid input
 
-//   // 3. Blur validation (most important)
-//   - [ ] Auto-corrects to MIN_SERVINGS (1) when empty
-//   - [ ] Auto-corrects to MIN_SERVINGS when value < 1
-//   - [ ] Auto-corrects to MAX_SERVINGS (12) when value > 12
-//   - [ ] Keeps valid value (7) unchanged on blur
+  // - [ ] Calls onChange when typing valid number (5)
+  // - [ ] Updates display value immediately
 
-//   // 4. Edge cases
-//   - [ ] Allows temporary invalid input while typing
-//   - [ ] Handles non-numeric input
-// }
+  // // 3. Blur validation (most important)
+  // - [ ] Auto-corrects to MIN_SERVINGS (1) when empty
+  // - [ ] Auto-corrects to MIN_SERVINGS when value < 1
+  // - [ ] Auto-corrects to MAX_SERVINGS (12) when value > 12
+  // - [ ] Keeps valid value (7) unchanged on blur
+
+  // // 4. Edge cases
+  // - [ ] Allows temporary invalid input while typing
+  // - [ ] Handles non-numeric input
+});
